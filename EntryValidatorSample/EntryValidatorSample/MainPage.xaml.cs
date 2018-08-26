43c0a9ba6e11a57e57e7fa28397599f626ce5bcc
Ahmed Aglan <aglan@live.com>
2018-08-26 17:46:03 +0200

    Add Sample

---
+++ EntryValidatorSample/EntryValidatorSample/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace EntryValidatorSample
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+    async void btnValidate_Clicked(object sender, EventArgs e)
+    {
+      if (!ValidateForm())
+      {
+       await DisplayAlert("Validate", "Please enter required data", "OK");
+      }
+      else
+      {
+        await DisplayAlert("Validate", "Data saved successfully", "OK");
+      }
+    }
+    private bool ValidateForm()
+    {
+      return
+        VldtrName.Validate() &
+        VldtrAddress.Validate() &
+        VldtrEmail.Validate() &
+        VldtrPhone.Validate() &
+        VldtrWeight.Validate() &
+        VldtrRegExp.Validate();
+    }
+
+  }
+}
